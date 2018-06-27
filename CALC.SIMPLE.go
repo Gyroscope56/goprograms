2d6074314a27da6fd9ac0fcc0c5e0cbb0a8165eb
Gyroscope56 <hi2arvindkumar@gmail.com>
2018-06-26 21:06:28 -0700

    Added scanner to read tokens

--- CALC.SIMPLE.go
+++ CALC.SIMPLE.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
+func readFromKeyboard() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+	for scanner.Scan() {
+		str := scanner.Text()
+		fmt.Println(str)
+	}
+
+	// intgr := 0
+	// fmt.Scanf("%d", &intgr)
+	// op := ' '
+	// fmt.Scanf("%c", &op)
+	// fmt.Scanf("\n")
+	// fmt.Println(intgr, op)
 }
